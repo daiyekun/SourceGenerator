181815eb060c66e2f45356d44f73f5c85aeda483
daiyekun <daiyekun@qq.com>
2023-04-09 17:16:58 +0800

    源代码生成项目dome第一次提交

---
+++ SourceGeneratorDome1/GreetingUsePartialClassm.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+namespace GreetingTest;
+
+
+public static partial class GreetingUsePartialClass
+{
+    //public static void hello(string name)
+    //{
+    //    SayHelloTo1( name);
+    //}
+    public  static partial void SayHelloTo1(string name);
+}
+
---
+++ SourceGeneratorDome1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GreetingTest;
+Console.WriteLine("Hello, World!");
+Greeting.SayHelloTo("dyk");
+les2_GreetingGeneratorMethod();
+Console.ReadKey();
+
+
+static void les2_GreetingGeneratorMethod() => GreetingUsePartialClass.SayHelloTo1("dyk");
+
